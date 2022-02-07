914652659f695cba0ba09bef199b6d1e2747ac54
谷宇维 <molixianggu@gmail.com>
2022-02-07 17:49:22 +0800

    Init build

---
+++ web/assets/tiled/move_01.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="move_01" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <properties>
+  <property name="aa" value="asdf32"/>
+ </properties>
+ <image source="../textures/move_01.png" width="192" height="256"/>
+</tileset>
---
+++ web/assets/tiled/冰雪.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="冰雪" tilewidth="32" tileheight="32" tilecount="496" columns="8">
+ <image source="../textures/tiled_map/冰雪.png" width="256" height="1984"/>
+</tileset>
